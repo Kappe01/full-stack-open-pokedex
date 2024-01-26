69d300993d2fa9b9065636c0e73537544d44267d
Kasper Envalds <kasper.envalds@gmail.com>
2024-01-26 21:59:31 +0200

    fix

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
-app.listen(PORT + 1, () => {
+app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
